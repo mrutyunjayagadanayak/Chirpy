e42d2b32501af21f10f26b0016e62568f5a1fc3c
bapun <mrutyunjayagadanayak@yandex.com>
2025-05-07 22:22:54 +0530

    start server

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import "net/http"
+
+func main() {
+	servMux := http.NewServeMux()
+	server := http.Server{
+		Addr:    ":8080",
+		Handler: servMux,
+	}
+	server.ListenAndServe()
+}
