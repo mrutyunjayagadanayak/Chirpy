92cce3256b19d8e1bb411cec43dbe2896be0ec65
bapun <mrutyunjayagadanayak@yandex.com>
2025-05-09 00:08:33 +0530

    File server

--- main.go
+++ main.go
@@ -1,12 +1,19 @@
 package main
 
-import "net/http"
+import (
+	"fmt"
+	"net/http"
+)
 
 func main() {
 	servMux := http.NewServeMux()
+	servMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{
 		Addr:    ":8080",
 		Handler: servMux,
 	}
-	server.ListenAndServe()
+	err := server.ListenAndServe()
+	if err != nil {
+		fmt.Printf("%v", err)
+	}
 }
